d7c5e21b182d8f5180a85fd6d60fd833ac3ee038
anshkhare1607-web <anshkhare1607@gmail.com>
2026-04-16 11:42:06 +0530

    Refactor : Updated

---
+++ ShoppingApp/src/main/java/com/shoppingapp/main/dto/OrderItemResponseDto.java
@@ -0,0 +1,29 @@
+package com.shoppingapp.main.dto;
+
+
+public class OrderItemResponseDto {
+    private Long productId;
+    private Integer quantity;
+    private Double unitPrice;
+
+    public OrderItemResponseDto() {}
+
+    public Long getProductId() { 
+    	return productId; 
+    }
+    public void setProductId(Long productId) { 
+    	this.productId = productId;
+    }
+    public Integer getQuantity() { 
+    	return quantity; 
+    }
+    public void setQuantity(Integer quantity) { 
+    	this.quantity = quantity; 
+    }
+    public Double getUnitPrice() { 
+    	return unitPrice; 
+    }
+    public void setUnitPrice(Double unitPrice) { 
+    	this.unitPrice = unitPrice; 
+    }
+}
