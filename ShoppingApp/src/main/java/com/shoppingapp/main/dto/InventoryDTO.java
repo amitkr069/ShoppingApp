abd1c9b16f943683dc60615a6553a8ecba0e6466
amitkr069 <aman3133@7-11.com>
2026-04-16 17:22:37 +0530

    Updated the folder structure

--- ShoppingApp/src/main/java/com/shoppingapp/main/dto/InventoryDTO.java
+++ ShoppingApp/src/main/java/com/shoppingapp/main/dto/InventoryDTO.java
@@ -19,15 +19,31 @@ public class InventoryDTO {
 
     public InventoryDTO() {}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() { 
+    	return id; 
+    }
+    public void setId(Long id) { 
+    	this.id = id; 
+    }
 
-    public Integer getStockQuantity() { return stockQuantity; }
-    public void setStockQuantity(Integer stockQuantity) { this.stockQuantity = stockQuantity; }
+    public Integer getStockQuantity() { 
+    	return stockQuantity; 
+    	}
+    public void setStockQuantity(Integer stockQuantity) { 
+    	this.stockQuantity = stockQuantity; 
+    	}
 
-    public Integer getThreshold() { return threshold; }
-    public void setThreshold(Integer threshold) { this.threshold = threshold; }
+    public Integer getThreshold() { 
+    	return threshold; 
+    	}
+    public void setThreshold(Integer threshold) { 
+    	this.threshold = threshold; 
+    	}
 
-    public Long getProductId() { return productId; }
-    public void setProductId(Long productId) { this.productId = productId; }
+    public Long getProductId() { 
+    	return productId; 
+    	}
+    public void setProductId(Long productId) { 
+    	this.productId = productId; 
+    	}
 }
