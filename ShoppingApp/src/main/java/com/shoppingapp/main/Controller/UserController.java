3446705c5d7b301f2a6b864b84d0e4ee3a4fdb38
harshpateriya12 <harsh.pateriya1@gmail.com>
2026-04-17 14:06:05 -0700

    Coded for controller in User Module

---
+++ ShoppingApp/src/main/java/com/shoppingapp/main/Controller/UserController.java
@@ -0,0 +1,53 @@
+package com.shoppingapp.main.Controller;
+
+import com.shoppingapp.main.dto.UserRequestDto;
+import com.shoppingapp.main.dto.UserResponseDto;
+import com.shoppingapp.main.Service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto request) {
+        UserResponseDto response = userService.createUser(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+  
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        UserResponseDto user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id,
+                                                      @RequestBody UserRequestDto request) {
+        UserResponseDto updatedUser = userService.updateUser(id, request);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+   
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+}
